0c5bbbe6055c8dd4d85f7701cdfc431dde2c47aa
Loaming <67550518+Loaming@users.noreply.github.com>
2021-03-12 02:51:28 +0300

    op menu

--- scripts.js
+++ scripts.js
@@ -2,13 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
   let btn1 = document.querySelector(".btn-menu-1"),
       btn2 = document.querySelector(".btn-menu-2"),
-      nav = document.querySelector(".nav-mob-menu"),
+      nav = document.querySelector(".nav"),
+      navMenu = document.querySelector(".nav-mob-menu"),
       menu = document.querySelector(".mob-menu");
 
 menu.onclick = function() {
   btn1.classList.toggle('close');
   btn2.classList.toggle('open');
-  nav.classList.toggle('open-nav');
+  nav.classList.toggle('nav-op');
+  navMenu.classList.toggle('open-nav');
   if(btn1.classList.contains('close')){
     document.body.style.overflow='hidden';
   }else{
